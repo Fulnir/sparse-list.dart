175f9e3242359bda222c6afb1b4636a78276c86c
Edwin Bühler <fulnir@gmail.com>
2013-12-27 17:12:49 +0100

    fix file name for junit test

--- test/sparse_list_test_junit.dart
+++ test/sparse_list_test_junit.dart
@@ -3,7 +3,7 @@
 library sparse_list_test_junit;
 
 import 'package:junitconfiguration/junitconfiguration.dart';
-import 'sparse_lis_test.dart' as all_tests;
+import 'sparse_list_test.dart' as all_tests;
 
 void main() {
   JUnitConfiguration.install();
